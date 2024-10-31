097cac818d454c20b6f067eaffe814aba54044f4
Sentric Nova <sentricnova@gmail.com>
2024-10-31 19:43:15 +0530

    Add files via upload

--- core/pluiginsx/restriction_saver.py
+++ core/pluiginsx/restriction_saver.py
@@ -17,6 +17,9 @@ from pyrogram.errors import FloodWait
 from pyrogram.enums import ChatType
 
 
+is_busy = False
+
+
 async def valid_url(url):
     try:
         result = urllib.parse.urlparse(url)
@@ -31,34 +34,33 @@ async def valid_url(url):
         return f"While validating URL. The Following Issue Happened:\n{err}\n\n**HOW TO SOLVE :**\n- Check If you gave a wrong url.\n- A url looks like : https://t.me/social_bots"
 
 
-async def channel_address_lookups(text):
-    lower_text = text.lower()
-    is_url = await valid_url(text)
-    if is_url=="valid":
-        split_url = text.split('/')
-        if lower_text.startswith("https://t.me/c/"):
-            try:
-                return int(f"-100{split_url[-2]}"), "needlink", int(split_url[-1])
-            except:
-                return int(f"-100{split_url[-2]}"), "needlink", False
-        
-        elif lower_text.startswith("https://t.me/"):
-            if len(split_url) > 2:
-                return f"@{split_url[-2]}", "public", int(split_url[-1])
-            else:
-                return f"@{split_url[-2]}", "public", False
-        elif lower_text.startswith("https://t.me/+"):
-            entity = lower_text.replace("https://t.me/+", "https://t.me/joinchat/")
-            return entity, "private", False
-        elif lower_text.startswith("@"):
-            return lower_text, "public", False
-        else:
-            return False, False, False
-    elif lower_text.lstrip("-").isdigit() or lower_text.isdigit():
-        return int(lower_text), "channel_id", False
-    else:
-        return False, False, False
 
+async def url_parse(link):
+    valid = await valid_url(link)
+    if valid != "valid":
+        return False, False, False
+    modified_string = link.replace("https://t.me", "").lstrip("/")
+    shorted_link = [part for part in modified_string.strip().split("/") if part]
+    if shorted_link[0].lower() == "c":
+        if len(shorted_link) == 4:
+            if shorted_link[-3].startswith("-100"):
+                return int(shorted_link[-3]), int(shorted_link[-2]), int(shorted_link[-1]), "private"
+            else:
+                return int("-100" + shorted_link[-3]), int(shorted_link[-2]), int(shorted_link[-1]), "private"
+        elif len(shorted_link) == 3:
+            if shorted_link[-3].startswith("-100"):
+                return int(shorted_link[-2]), False, int(shorted_link[-1]), "private"
+            else:
+                return int("-100" + shorted_link[-2]), False, int(shorted_link[-1]), "private"
+        else:
+            return False, False, False, "invalid"
+    elif len(shorted_link) >= 2:
+        if len(shorted_link) == 3:
+            return f"@{shorted_link[-3]}", int(shorted_link[-2]), int(shorted_link[-1]), "public"
+        else:
+            return f"@{shorted_link[-2]}", False, int(shorted_link[-1]), "public"
+    else:
+        False, False, False, "Invalid"
 
 
 
@@ -74,42 +76,78 @@ async def save(client, message, ub=userbot):
         pure_msgs = [] # same to this
         start_from = msg_splited[1]
         end_here = msg_splited[2]
-        chat_id_1, chat_type_1, start_msg_id = await channel_address_lookups(start_from)
-        chat_id_2, chat_type_2, end_msg_id = await channel_address_lookups(end_here)
+        chat_id_1, topic_id, ostart_msg_id, chat_type_1 = await url_parse(start_from)
+        print(chat_id_1, topic_id, ostart_msg_id, chat_type_1)
+        chat_id_2, topic_id, oend_msg_id, chat_type_2 = await url_parse(end_here)
+        print(chat_id_2, topic_id, oend_msg_id, chat_type_2)
+        if ostart_msg_id > oend_msg_id:
+            start_msg_id = oend_msg_id
+            end_msg_id = ostart_msg_id
+        else:
+            start_msg_id = ostart_msg_id
+            end_msg_id = oend_msg_id
+
         if chat_id_1 != chat_id_2:
             return await message.reply_text("LOL! You gave me two diff channels/groups's link. Check your given urls.")
         
-        if chat_type_1=="private":
-            try:
-                await ub.join_chat(chat_type_2)
-                return await message.reply_text("Chat joined! Now try again with message links.")
-            except FloodWait as t:
-                await asyncio.sleep(t.value)
-            except Exception as err:
-                return await message.reply_text(err)
         if not start_msg_id or not end_msg_id:
             return await message.reply_text("You didn't gave message link from where I'll start saving and from where I'll stop saving!\n\n**EXAMPLE:**\n/save https://t.me/msg_link/23432 https://t.me/msg_link/23440")
 
         try:
             info = await message.reply_text("⏳ **Loading message ids...**")
-            async for messagex in ub.get_chat_history(chat_id=chat_id_1, min_id=start_msg_id, max_id=end_msg_id):
-                if messagex.media:
-                    pure_msgs.append(messagex)
-                    msgs_ids.append(messagex.id)
+            print(1)
+            if topic_id:
+                print(2)
+                useless_list = []
+                print(3)
+                await message.reply_text("**TOPIC WISE GROUP FOUND!**, Bot will work slow if there are many deleted files.")
+                async for messagex in ub.get_discussion_replies(chat_id_1, topic_id):
+                    if messagex.media:
+                        pure_msgs.append(messagex)
+                        msgs_ids.append(messagex.id)
+                await info.edit_text(f"{len(msgs_ids)} media msgs are Loaded.")
+            else:
+                print(4, chat_id_1, start_msg_id, end_msg_id)
+                try:
+                    if chat_type_1 == "public":
+                        chat_fetch = await app.get_chat(chat_id_1)
+                        chat_id_541 = chat_fetch.id
+                    else:
+                        chat_fetch = await ub.get_chat(chat_id_1)
+                        chat_id_541 = chat_fetch.id
+                except Exception as err:
+                    await message.reply_text(f"Error Found But Bot is still trying!\n\n{err}")
+                    chat_id_541 = chat_id_1
+                try:
+                    async for messagex in ub.get_chat_history(chat_id=chat_id_1, min_id=start_msg_id, max_id=end_msg_id):
+                        print(messagex)
+                        if messagex.media:
+                            pure_msgs.append(messagex)
+                            msgs_ids.append(messagex.id)
+                    print(5)
+                except Exception as err:
+                    print(err)
+                    pass
             await info.edit_text(f"⌛ **{len(msgs_ids)} Media Messages are loaded! Trying to save...**")
             if chat_type_1 == "public":
+                print("running public protocol")
+                if not msgs_ids:
+                    return await message.reply_text("Userbot not loaded chat history of this public channel!\n\nShould we direct start forwarding?")
                 for save_msg_id in msgs_ids:
+                    print(123)
                     try:
+                        print(234)
                         await app.copy_message(message.chat.id, chat_id_1, save_msg_id)
                         saved += 1
                     except FloodWait as t:
+                        print(3254)
                         await asyncio.sleep(t.value)
                         await app.copy_message(message.chat.id, chat_id_1, save_msg_id)
                         saved += 1
                     except Exception as err:
-                        print(err)
+                        print(453)
                         failed += 1
-                return await info.edit_text(f"{saved} every type of media files are saved {f'and {failed} failed' if failed else ''}.")
+                return await message.reply_text(f"{saved} every type of media files are saved {f'and {failed} failed' if failed else ''}.")
             else:
                 for mmsg in pure_msgs:
                     try:
@@ -134,10 +172,9 @@ async def save(client, message, ub=userbot):
                         except Exception as err:
                             pass
                     except Exception as err:
-                        print(err)
                         failed += 1
                         pass
-                return await info.edit_text(f"{saved} every type of media files are saved {f'and {failed} failed' if failed else None}.")
+                return await message.reply_text(f"{saved} every type of media files are saved {f'and {failed} failed' if failed else None}.")
         except FloodWait as t:
             await asyncio.sleep(t.value)
         except Exception as err:
@@ -148,7 +185,8 @@ async def save(client, message, ub=userbot):
                 pass
             return
     elif len(msg_splited) == 2:
-        chat_id_1, chat_type_1, start_msg_id = await channel_address_lookups(msg_splited[1])
+        chat_id_1, topic_id, start_msg_id, chat_type_1 = await url_parse(msg_splited[1])
+        #chat_id_1, chat_type_1, start_msg_id = await channel_address_lookups()
         if not start_msg_id or not chat_id_1:
             return await message.reply_text("This is an invalid URL!")
         info = await message.reply_text("⏳ **Loading message ids...**")
@@ -161,7 +199,6 @@ async def save(client, message, ub=userbot):
                 await app.copy_message(message.chat.id, chat_id_1, start_msg_id)
                 saved += 1
             except Exception as err:
-                return await message.reply_text(err)
                 failed += 1
             return await info.edit_text(f"{saved} every type of media files are saved {f'and {failed} failed' if failed else ''}.")
         else:
@@ -187,7 +224,6 @@ async def save(client, message, ub=userbot):
                 except Exception as err:
                     pass
             except Exception as err:
-                return await message.reply_text(err)
                 failed += 1
                 pass
             return await info.edit_text(f"{saved} every type of media files are saved {f'and {failed} failed' if failed else ''}.")
